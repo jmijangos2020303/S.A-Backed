ea48cae304d17ba6ff192c40e41be36b99dbe69a
José Flavio Xavier Mijangos Vasquez <jmijangos-2020303@kinal.edu.gt>
2022-08-01 11:47:27 -0600

    mongo atlas

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const control = require("./src/controllers/usuario.controller")
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Contrataciones', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect('mongodb+srv://contrataciones:123@cluster0.5fwru.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Se ha conectado correctamente a la base de datos.');
 
     control.RegistrarAd();
